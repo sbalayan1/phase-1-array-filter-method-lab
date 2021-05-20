57c0d67f6c8b30ed65a4f80b0cc670d6b6aa312c
sbalayan1 <balayans2014@yahoo.com>
2021-05-19 17:23:37 -0700

    commit tree a703148fa5049e8f59b9c9f4d5b7a8fb131de202

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+function findMatching (someArray, someString) {
+    return someArray.filter(item => item.toUpperCase() === someString.toUpperCase())
+ 
+}
+
+
+function fuzzyMatch(someArray,someString) {
+    return someArray.filter(item => item.charAt(0)=== someString.charAt(0))
+   
+}
+
+function matchName(someArray, someString) {
+    return someArray.filter(element => element.name === someString)
+}
